73f4e3059036156f7f3a2b9af28edd3aa08833c4
Philippe Martin <phmartin@redhat.com>
2024-08-28 09:29:10 +0200

    feat: cache layers on disk
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ src/cache.ts
@@ -0,0 +1,136 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import type { ImageFilesystemLayers, ImageInfo } from '@podman-desktop/api';
+import { mkdir, readdir, readFile, rm, utimes, writeFile } from 'fs/promises';
+import * as path from 'node:path';
+import * as zlib from 'node:zlib';
+import { promisify } from 'node:util';
+import type { Preferences } from './preferences';
+import { statSync } from 'node:fs';
+
+const gzip = promisify(zlib.gzip);
+const gunzip = promisify(zlib.gunzip);
+
+export interface CacheFileInfo {
+  name: string;
+  size: number;
+}
+
+export class Cache {
+  constructor(
+    private preferences: Preferences,
+    private path: string,
+  ) {}
+
+  public async init() {
+    await this.limitSize(0);
+  }
+
+  // Returns the cached data for an image
+  // or undefined if the data is not cached for this image
+  // If data is cached, modify the access time of the file
+  public async get(image: ImageInfo): Promise<ImageFilesystemLayers | undefined> {
+    try {
+      const filepath = this.getImageCacheFile(image);
+      const compressed = await readFile(filepath);
+      try {
+        await utimes(filepath, new Date(), new Date());
+      } catch (err: unknown) {
+        console.warn(`unable to modify atime and mtime for ${image.Id} cache file: ${err}`);
+      }
+      const content = await gunzip(compressed);
+      return JSON.parse(content.toString());
+    } catch (error: unknown) {
+      if (this.isErrorWithCode(error) && error.code === 'ENOENT') {
+        return undefined;
+      } else {
+        console.warn(`error getting/reading cache for ${image.Id}`);
+        return undefined;
+      }
+    }
+  }
+
+  // cache data for an image and removes older cache files to remain in the cache size limit
+  public async save(image: ImageInfo, layers: ImageFilesystemLayers): Promise<void> {
+    const compressed = await gzip(JSON.stringify(layers));
+    await this.limitSize(compressed.length);
+    if (compressed.length <= 1024 * 1024 * this.preferences.getCacheSize()) {
+      const filepath = this.getImageCacheFile(image);
+      const basedir = path.dirname(filepath);
+      await mkdir(basedir, { recursive: true });
+      await writeFile(filepath, compressed);
+    }
+  }
+
+  // Remove older files to limit the size to the cache
+  // reserved is the amount of space to remove from the cache limit
+  // to keep the place for a new file to be wriiten
+  public async limitSize(reserved: number): Promise<void> {
+    const rootdir = this.getRootDir();
+    const files = await this.getSortedFilesByAtime(rootdir);
+    const max = 1024 * 1024 * this.preferences.getCacheSize();
+    let acc = reserved;
+    for (const file of files) {
+      if (acc + file.size > max) {
+        await this.deleteCacheFile(file.name);
+        continue;
+      }
+      acc += file.size;
+    }
+  }
+
+  public async clearImageCache(id: string): Promise<void> {
+    return this.deleteCacheFile(`sha256:${id}.gz`);
+  }
+
+  public async deleteCacheFile(filename: string): Promise<void> {
+    await rm(path.join(this.getRootDir(), filename));
+  }
+
+  private getRootDir(): string {
+    return path.join(this.path, 'cache', 'v1');
+  }
+
+  private getImageCacheFile(image: ImageInfo): string {
+    return path.join(this.getRootDir(), image.Id + '.gz');
+  }
+
+  private isErrorWithCode(err: unknown): err is Error & { code: unknown } {
+    return err instanceof Error && 'code' in err;
+  }
+
+  // returns the list of cache files, sorted by access time, last accessed first
+  public async getSortedFilesByAtime(dir: string): Promise<CacheFileInfo[]> {
+    const files = await readdir(dir);
+    return files
+      .map(fileName => {
+        const stats = statSync(`${dir}/${fileName}`);
+        return {
+          name: fileName,
+          atime: stats.atime.getTime(),
+          size: stats.size,
+        };
+      })
+      .sort((a, b) => b.atime - a.atime)
+      .map(file => ({
+        name: file.name,
+        size: file.size,
+      }));
+  }
+}
--- src/explorer.spec.ts
+++ src/explorer.spec.ts
@@ -16,11 +16,12 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import { expect, test, vi } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { Explorer } from './explorer';
 import { containerEngine } from '@podman-desktop/api';
 import { cp } from 'node:fs/promises';
 import * as path from 'node:path';
+import type { Cache } from './cache';
 
 vi.mock('@podman-desktop/api', async () => {
   return {
@@ -30,7 +31,8 @@ vi.mock('@podman-desktop/api', async () => {
   };
 });
 
-test('getFilesystemLayers', async () => {
+describe('getFilesystemLayers', () => {
+  let explorer: Explorer;
   const provider = {
     addFile: vi.fn(),
     addDirectory: vi.fn(),
@@ -39,64 +41,137 @@ test('getFilesystemLayers', async () => {
     addOpaqueWhiteout: vi.fn(),
     dispose: vi.fn(),
   };
-  const explorer = new Explorer();
-  explorer.setProvider(provider);
+  const cacheGetMock = vi.fn();
+  const cacheSaveMock = vi.fn();
+  const cache = {
+    get: cacheGetMock,
+    save: cacheSaveMock,
+  } as unknown as Cache;
 
-  vi.mocked(containerEngine).saveImage.mockImplementation(
-    async (_engineId: string, _imageId: string, tarFile: string) => {
-      await cp(path.join(__dirname, '../tests/archive.tar'), tarFile);
-    },
-  );
-  const result = await explorer.getFilesystemLayers({
-    engineId: '',
-    engineName: '',
-    Id: 'id',
-    ParentId: 'parentId',
-    RepoTags: [],
-    Created: 1,
-    Size: 1,
-    VirtualSize: 1,
-    SharedSize: 1,
-    Labels: {},
-    Containers: 1,
-    Digest: '',
+  beforeEach(() => {
+    explorer = new Explorer(cache);
+    explorer.setProvider(provider);
   });
-  const layers = [
-    {
-      id: '8e13bc96641a',
-      createdBy: 'BusyBox 1.36.1 (glibc), Debian 12',
-    },
-    { id: 'e4423e7382d4', createdBy: '/bin/sh -c echo -n 1 > 1.txt' },
-    { id: '6f35d54b965c', createdBy: '/bin/sh -c echo -n 12 > 2.txt' },
-    { id: '0cb105fbebcc', createdBy: '/bin/sh -c echo -n 123 > 3.txt' },
-    { id: '651b9c981348', createdBy: '/bin/sh -c rm 2.txt' },
-  ];
-  expect(result).toEqual({
-    layers,
+
+  test('getFilesystemLayers without cache', async () => {
+    cacheGetMock.mockReturnValue(undefined);
+
+    vi.mocked(containerEngine).saveImage.mockImplementation(
+      async (_engineId: string, _imageId: string, tarFile: string) => {
+        await cp(path.join(__dirname, '../tests/archive.tar'), tarFile);
+      },
+    );
+    const result = await explorer.getFilesystemLayers({
+      engineId: '',
+      engineName: '',
+      Id: 'id',
+      ParentId: 'parentId',
+      RepoTags: [],
+      Created: 1,
+      Size: 1,
+      VirtualSize: 1,
+      SharedSize: 1,
+      Labels: {},
+      Containers: 1,
+      Digest: '',
+    });
+    const layers = [
+      {
+        id: '8e13bc96641a',
+        createdBy: 'BusyBox 1.36.1 (glibc), Debian 12',
+      },
+      { id: 'e4423e7382d4', createdBy: '/bin/sh -c echo -n 1 > 1.txt' },
+      { id: '6f35d54b965c', createdBy: '/bin/sh -c echo -n 12 > 2.txt' },
+      { id: '0cb105fbebcc', createdBy: '/bin/sh -c echo -n 123 > 3.txt' },
+      { id: '651b9c981348', createdBy: '/bin/sh -c rm 2.txt' },
+    ];
+    expect(result).toEqual({
+      layers,
+    });
+    expect(provider.addDirectory).toHaveBeenCalledWith(layers[0], {
+      mode: 0o755,
+      path: 'lib/',
+    });
+    expect(provider.addSymlink).toHaveBeenCalledWith(layers[0], {
+      mode: 0o777,
+      path: 'lib64',
+      linkPath: 'lib',
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers[1], {
+      mode: 0o644,
+      path: '1.txt',
+      size: 1,
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers[2], {
+      mode: 0o644,
+      path: '2.txt',
+      size: 2,
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers[3], {
+      mode: 0o644,
+      path: '3.txt',
+      size: 3,
+    });
+    expect(provider.addWhiteout).toHaveBeenCalledWith(layers[4], '2.txt');
   });
-  expect(provider.addDirectory).toHaveBeenCalledWith(layers[0], {
-    mode: 0o755,
-    path: 'lib/',
+
+  test('getFilesystemLayers with cache', async () => {
+    const layers = {
+      layers: [
+        {
+          id: '8e13bc96641a',
+          createdBy: 'BusyBox 1.36.1 (glibc), Debian 12',
+        },
+        { id: 'e4423e7382d4', createdBy: '/bin/sh -c echo -n 1 > 1.txt' },
+        { id: '6f35d54b965c', createdBy: '/bin/sh -c echo -n 12 > 2.txt' },
+        { id: '0cb105fbebcc', createdBy: '/bin/sh -c echo -n 123 > 3.txt' },
+        { id: '651b9c981348', createdBy: '/bin/sh -c rm 2.txt' },
+      ],
+    };
+
+    cacheGetMock.mockReturnValue(layers);
+
+    const result = await explorer.getFilesystemLayers({
+      engineId: '',
+      engineName: '',
+      Id: 'id',
+      ParentId: 'parentId',
+      RepoTags: [],
+      Created: 1,
+      Size: 1,
+      VirtualSize: 1,
+      SharedSize: 1,
+      Labels: {},
+      Containers: 1,
+      Digest: '',
+    });
+    expect(result).toEqual({
+      layers: layers.layers,
+    });
+    expect(provider.addDirectory).toHaveBeenCalledWith(layers.layers[0], {
+      mode: 0o755,
+      path: 'lib/',
+    });
+    expect(provider.addSymlink).toHaveBeenCalledWith(layers.layers[0], {
+      mode: 0o777,
+      path: 'lib64',
+      linkPath: 'lib',
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers.layers[1], {
+      mode: 0o644,
+      path: '1.txt',
+      size: 1,
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers.layers[2], {
+      mode: 0o644,
+      path: '2.txt',
+      size: 2,
+    });
+    expect(provider.addFile).toHaveBeenCalledWith(layers.layers[3], {
+      mode: 0o644,
+      path: '3.txt',
+      size: 3,
+    });
+    expect(provider.addWhiteout).toHaveBeenCalledWith(layers.layers[4], '2.txt');
   });
-  expect(provider.addSymlink).toHaveBeenCalledWith(layers[0], {
-    mode: 0o777,
-    path: 'lib64',
-    linkPath: 'lib',
-  });
-  expect(provider.addFile).toHaveBeenCalledWith(layers[1], {
-    mode: 0o644,
-    path: '1.txt',
-    size: 1,
-  });
-  expect(provider.addFile).toHaveBeenCalledWith(layers[2], {
-    mode: 0o644,
-    path: '2.txt',
-    size: 2,
-  });
-  expect(provider.addFile).toHaveBeenCalledWith(layers[3], {
-    mode: 0o644,
-    path: '3.txt',
-    size: 3,
-  });
-  expect(provider.addWhiteout).toHaveBeenCalledWith(layers[4], '2.txt');
 });
